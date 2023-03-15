c3e853844b9709ee3dd10685c9659a13b3d78756
balahari <balaharisankar2003@gmail.com>
2023-03-14 21:09:30 -0700

    Backend till 14 Mar 23

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,13 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(express.json());
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', 'https://linkbowl.netlify.app');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+  });
 
 const auth = require('./routers/auth');
 const user=require('./routers/user');
