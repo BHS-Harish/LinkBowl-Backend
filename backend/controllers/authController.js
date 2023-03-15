1ffb268af5abaa2e5cd54bec530283453171c1f6
balahari <balaharisankar2003@gmail.com>
2023-03-14 21:00:07 -0700

    Backend till 14 Mar 23

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -107,7 +107,7 @@ exports.loginUser=catchAsyncError(async (req,res,next)=>{
     const options={
         expires:new Date(Date.now()+process.env.COOKIE_EXPIRE_TIME*24*60*60*1000),
         httpOnly:true,
-        sameSite:"lax",
+        sameSite:"none",
         secure:true
     }
     res.status(200).cookie('authToken',token,options).json({
