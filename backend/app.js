4ef9669fa00bc4bba6b95b5bc3b245e480869bbf
balahari <balaharisankar2003@gmail.com>
2023-04-12 11:10:16 -0700

    test

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,8 @@ const errorMiddleWare = require('./middleware/error');
 const cors = require('cors');
 const bodyParser=require('body-parser');
 const cookieParser=require('cookie-parser');
-
-app.use(cors({credentials:true,origin:'https://linkbowl.netlify.app'||'http://localhost:3000'}));
+//'https://linkbowl.netlify.app'
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
